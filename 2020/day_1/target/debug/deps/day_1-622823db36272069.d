60630736c0f901cd59f5d4a9fc595797758baa7a
moodytongytong <moodytongytong@users.noreply.github.com>
2022-03-11 14:07:07 +0000

    remove runnables

--- 2020/day_1/target/debug/deps/day_1-622823db36272069.d
+++
@@ -1,6 +0,0 @@
-/home/moodytongytong/Dropbox/personal/self_study_resources/CS/github/moodytongytong/adventofcode-rust/2020/day_1/target/debug/deps/day_1-622823db36272069: src/main.rs src/sum_finder.rs
-
-/home/moodytongytong/Dropbox/personal/self_study_resources/CS/github/moodytongytong/adventofcode-rust/2020/day_1/target/debug/deps/day_1-622823db36272069.d: src/main.rs src/sum_finder.rs
-
-src/main.rs:
-src/sum_finder.rs:
