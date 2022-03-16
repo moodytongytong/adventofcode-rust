a57a2be687a29db8b987b9890ec78f230c51cb35
Tong Luo <moodytongytong@users.noreply.github.com>
2022-03-16 15:01:54 +0000

    complete part 1 and refactored for part 2

--- 2020/day_6/src/custom_customs.rs
+++ 2020/day_6/src/custom_customs.rs
@@ -1,17 +1,64 @@
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
+use std::fs::read_to_string;
+use std::io;
+use std::collections::HashSet;
 
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
+pub fn create_data_by_groups(filepath: &str) -> Result<Vec<String>, io::Error> {
+    let raw_data = read_to_string(filepath)?;
+    Ok(raw_data.split("\n\n").map(|item| item.to_string()).collect())
 }
+
+pub fn find_sum_of_counts(data: Vec<String>, rule: &dyn Fn(&str) -> usize) -> usize {
+    data.iter().map(|group_data| find_pt1_count_from_group(group_data)).sum()
+}
+
+pub fn find_pt1_count_from_group(data: &str) -> usize {
+    let mut unique_chars = HashSet::<char>::new();
+    for c in data.chars() {
+        unique_chars.insert(c);
+    }
+    unique_chars.remove(&'\n');
+    unique_chars.len()
+}
+
+pub fn find_pt2_count_from_group(data: &str) -> usize {
+    3
+}
+
 
 #[cfg(test)]
 mod tests {
     
     use super::*;
+
+    #[test]
+    fn find_pt1_count_from_a_group() {
+        let group1_data = "abcx\nabcy\nabcz";
+        assert_eq!(6, find_pt1_count_from_group(group1_data));
+
+        let group2_data = "abc";
+        assert_eq!(3, find_pt1_count_from_group(group2_data));
+
+        let group2_data = "a\nb\nc";
+        assert_eq!(3, find_pt1_count_from_group(group2_data));
+    }
+
+    fn find_pt2_count_from_a_group() {
+        let group1_data = "abcx\nabcy\nabcz";
+        assert_eq!(3, find_pt2_count_from_group(group1_data));
+    }
+
+    #[test]
+    fn data_correctly_created() -> Result<(), io::Error>{
+        let data = create_data_by_groups("test_data/test1.txt")?;
+        assert_eq!(5, data.len());
+        Ok(())
+    }
+
+    #[test]
+    fn find_sum_of_all_counts_from_plane() {
+        let data = create_data_by_groups("test_data/test1.txt").unwrap();
+        assert_eq!(11, find_sum_of_counts(data, &find_pt1_count_from_group));
+    }
 
 
 }
--- 2020/day_6/src/main.rs
+++ 2020/day_6/src/main.rs
@@ -1,4 +1,13 @@
 mod custom_customs;
+use custom_customs::{
+    create_data_by_groups,
+    find_sum_of_counts,
+    find_pt1_count_from_group,
+};
+use std::io;
 
-fn main(){
+fn main() -> Result<(), io::Error>{
+    let data = create_data_by_groups("test_data/input.txt")?;
+    println!("The sum of counts from the flight is {}", find_sum_of_counts(data, &find_pt1_count_from_group));
+    Ok(())
 }
