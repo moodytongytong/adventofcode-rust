4f4e255c4b30d00bac7ebf50756899a591dd4a5e
moodytongytong <moodytongytong@users.noreply.github.com>
2022-03-25 11:16:47 +0000

    refactored day 6

--- 2020/day_6/src/custom_customs.rs
+++ 2020/day_6/src/custom_customs.rs
@@ -1,13 +1,16 @@
 use std::fs::read_to_string;
 use std::io;
-use std::collections::HashSet;
+use std::collections::{
+    HashSet,
+    HashMap,
+};
 
 pub fn create_data_by_groups(filepath: &str) -> Result<Vec<String>, io::Error> {
     let raw_data = read_to_string(filepath)?;
     Ok(raw_data.split("\n\n").map(|item| item.to_string()).collect())
 }
 
-pub fn find_sum_of_counts(data: Vec<String>, rule: &dyn Fn(&str) -> usize) -> usize {
+pub fn find_sum_of_counts(data: &Vec<String>, rule: &dyn Fn(&str) -> usize) -> usize {
     data.iter().map(|group_data| rule(group_data)).sum()
 }
 
@@ -20,9 +23,22 @@ pub fn find_pt1_count_from_group(data: &str) -> usize {
     unique_chars.len()
 }
 
-// pub fn find_pt2_count_from_group(data: &str) -> usize {
-//     3
-// }
+pub fn find_pt2_count_from_group(group_data: &str) -> usize {
+    let participants: Vec<&str> = group_data.split('\n').collect();
+    let mut answer_count = HashMap::<char, usize>::new();
+    for participant in &participants {
+        for c in participant.chars() {
+            if let Some(occurance) = answer_count.get_mut(&c) {
+                *occurance += 1;
+            } else  {
+                answer_count.insert(c, 1);
+            }
+        }
+    }
+    let participant_count = participants.len();
+    answer_count.values().filter(|&occurance| occurance == &participant_count).count()
+}
+
 
 
 #[cfg(test)]
@@ -42,10 +58,11 @@ mod tests {
         assert_eq!(3, find_pt1_count_from_group(group2_data));
     }
 
-    // fn find_pt2_count_from_a_group() {
-    //     let group1_data = "abcx\nabcy\nabcz";
-    //     assert_eq!(3, find_pt2_count_from_group(group1_data));
-    // }
+    #[test]
+    fn find_pt2_count_from_a_group() {
+        let group1_data = "abcx\nabcy\nabcz";
+        assert_eq!(3, find_pt2_count_from_group(group1_data));
+    }
 
     #[test]
     fn data_correctly_created() -> Result<(), io::Error>{
@@ -57,8 +74,6 @@ mod tests {
     #[test]
     fn find_sum_of_all_counts_from_plane() {
         let data = create_data_by_groups("test_data/test1.txt").unwrap();
-        assert_eq!(11, find_sum_of_counts(data, &find_pt1_count_from_group));
+        assert_eq!(11, find_sum_of_counts(&data, &find_pt1_count_from_group));
     }
-
-
 }
--- 2020/day_6/src/main.rs
+++ 2020/day_6/src/main.rs
@@ -3,11 +3,13 @@ use custom_customs::{
     create_data_by_groups,
     find_sum_of_counts,
     find_pt1_count_from_group,
+    find_pt2_count_from_group,
 };
 use std::io;
 
 fn main() -> Result<(), io::Error>{
     let data = create_data_by_groups("test_data/input.txt")?;
-    println!("The sum of counts from the flight is {}", find_sum_of_counts(data, &find_pt1_count_from_group));
+    println!("The sum of counts from the flight is {}", find_sum_of_counts(&data, &find_pt1_count_from_group));
+    println!("The sum of counts from the flight is {}", find_sum_of_counts(&data, &find_pt2_count_from_group));
     Ok(())
 }
