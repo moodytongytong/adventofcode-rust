ddaee9a97129f6e5f027dc98790e83235c9f2d4d
moodytongytong <moodytongytong@users.noreply.github.com>
2022-03-25 11:17:27 +0000

    setup day 7 directories

--- 2020/day_7/src/handy_haversacks.rs
+++ 2020/day_7/src/handy_haversacks.rs
@@ -2,6 +2,13 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
+struct Rule;
+
+impl Rule {
+    fn new(&self, line: &str) -> Self {
+        Rule
+    }
+}
 
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
@@ -14,5 +21,10 @@ where P: AsRef<Path>, {
 mod tests {
     
     use super::*;
+
+    #[test]
+    fn a_simple_rule_correctly_created() {
+        let rule = Rule::new("bright white bags contain 1 shiny gold bag.");
+    }
 
 }
