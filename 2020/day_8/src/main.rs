4e9fb65fa0f0906ab46bc64f9e4184ae3c6823a5
moodytongytong <moodytongytong@users.noreply.github.com>
2022-05-19 12:16:09 +0100

    setup day 8 directory

---
+++ 2020/day_8/src/main.rs
@@ -0,0 +1,7 @@
+mod handheld_halting;
+
+fn main() {
+    // let finder = Finder::new("./test_data/input.txt");
+    // println!("Answer to part 1 is {}", finder.find_product_of_two_targets_sum_to(2020));
+    // println!("Answer to part 2 is {}", finder.find_product_of_three_targets_sum_to(2020));
+}
