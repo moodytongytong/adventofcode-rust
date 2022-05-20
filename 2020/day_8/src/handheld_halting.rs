6a8bc47bdde62321eea7be7c0d2db6d43860dbc0
moodytongytong <moodytongytong@users.noreply.github.com>
2022-05-20 11:00:20 +0100

    complete day 8 part 1

--- 2020/day_8/src/handheld_halting.rs
+++ 2020/day_8/src/handheld_halting.rs
@@ -1,16 +1,16 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-// use std::collections::HashSet;
+use std::collections::HashSet;
 
-struct GameConsole {
+pub struct GameConsole {
     instructions: Vec<String>,
-    accumulator: usize,
+    accumulator: isize,
     position: usize,
 }
 
 impl GameConsole {
-    fn new(filepath: &str) -> Self {
+    pub fn new(filepath: &str) -> Self {
         let mut instructions = Vec::<String>::new();
         if let Ok(lines) = read_lines(filepath) {
             for line in lines {
@@ -27,13 +27,40 @@ impl GameConsole {
     }
 
     fn acc(&mut self, index: usize) {
-        let change: usize = self.instructions[index][4..].parse().unwrap();
+        let change: isize = self.instructions[index][4..].parse().unwrap();
         self.accumulator += change;
+        self.position += 1;
     }
 
     fn jmp(&mut self, index: usize) {
-        let change: usize = self.instructions[index][4..].parse().unwrap();
-        self.position = change + index;
+        let change: isize = self.instructions[index][4..].parse().unwrap();
+        self.position = (change + index as isize) as usize;
+    }
+
+    fn nop(&mut self, _index: usize) {
+        self.position += 1;
+    }
+
+    fn operate(&mut self, index: usize) {
+        let operation = &self.instructions[index][0..3];
+        match operation {
+            "acc" => self.acc(index),
+            "jmp" => self.jmp(index),
+            _ => self.nop(index),
+        }
+    }
+
+    fn next(&mut self) {
+        self.operate(self.position);
+    }
+
+    pub fn find_accumulator_before_loop(&mut self) -> isize {
+        let mut visited_indices = HashSet::<usize>::new();
+        while !visited_indices.contains(&self.position) {
+            visited_indices.insert(self.position);
+            self.next();
+        }
+        self.accumulator
     }
 }
 
@@ -65,5 +92,34 @@ mod tests {
         let mut console = GameConsole::new("test_data/test1.txt");
         console.jmp(2);
         assert_eq!(6, console.position);
+    }
+
+    #[test]
+    fn correctly_identify_operation() {
+        let mut console = GameConsole::new("test_data/test1.txt");
+        console.operate(1);
+        assert_eq!(1, console.accumulator);
+        console.operate(2);
+        assert_eq!(6, console.position);
+    }
+
+    #[test]
+    fn instructions_increments_correctly() {
+        let mut console = GameConsole::new("test_data/test1.txt");
+        console.next();
+        assert_eq!(0, console.accumulator);
+        assert_eq!(1, console.position);
+        console.next();
+        assert_eq!(1, console.accumulator);
+        assert_eq!(2, console.position);
+        console.next();
+        assert_eq!(1, console.accumulator);
+        assert_eq!(6, console.position);
+    }
+
+    #[test]
+    fn correctly_finds_accumulator_before_looping_back() {
+        let mut console = GameConsole::new("test_data/test1.txt");
+        assert_eq!(5, console.find_accumulator_before_loop());
     }
 }
--- 2020/day_8/src/main.rs
+++ 2020/day_8/src/main.rs
@@ -1,7 +1,9 @@
 mod handheld_halting;
+use handheld_halting::{
+    GameConsole
+};
 
 fn main() {
-    // let finder = Finder::new("./test_data/input.txt");
-    // println!("Answer to part 1 is {}", finder.find_product_of_two_targets_sum_to(2020));
-    // println!("Answer to part 2 is {}", finder.find_product_of_three_targets_sum_to(2020));
+    let mut console = GameConsole::new("test_data/input.txt");
+    println!("The answer to part 1 is {}", console.find_accumulator_before_loop());
 }
