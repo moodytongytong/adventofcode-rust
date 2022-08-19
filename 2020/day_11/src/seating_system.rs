9b352479e2a321ef018cd36eee0e243999483425
moodytongytong <moodytongytong@users.noreply@github.com>
2022-08-19 01:06:28 +0100

    incomplete day 11

--- 2020/day_11/src/seating_system.rs
+++ 2020/day_11/src/seating_system.rs
@@ -4,30 +4,24 @@ use std::path::Path;
 use std::collections::{
     HashMap,
     HashSet,
+    BTreeMap,
 };
 
-#[derive(PartialEq, Eq, Hash, Debug)]
+#[derive(PartialEq, Eq, Hash, Debug, Copy, Clone, PartialOrd, Ord)]
 struct Coord (usize, usize);
 
 impl Coord {
     fn get_neighbors(&self) -> HashSet<Coord> {
         let mut neighbors = HashSet::new();
-        if self.0 != 0 && self.1 != 0 { 
-            neighbors.insert(Coord(self.0 - 1, self.1 - 1));
+        let deltas : [(i8, i8); 8] = [(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)];
+        for delta in deltas {
+            let x = self.0 as i8 + delta.0;
+            let y = self.1 as i8 + delta.1;
+            if x >= 0 && y >= 0 {
+                neighbors.insert(Coord(x as usize, y as usize));
+            }
         }
-        if self.0 != 0 {
-            neighbors.insert(Coord(self.0 - 1, self.1));
-            neighbors.insert(Coord(self.0 - 1, self.1 + 1));
-        }
-        if self.1 != 0 {
-            neighbors.insert(Coord(self.0, self.1 - 1));
-            neighbors.insert(Coord(self.0 + 1, self.1 - 1));
-        }
-        neighbors.insert(Coord(self.0, self.1 + 1));
-        neighbors.insert(Coord(self.0 + 1, self.1));
-        neighbors.insert(Coord(self.0 + 1, self.1 + 1));
         neighbors
-        // What's a cleaner way to write this without getting stack overflow
     }
 }
 
@@ -48,47 +42,51 @@ impl WaitingArea {
     fn is_occupied(&self, location: &Coord) -> bool {
         if let Some(occupied) = self.seat_map.get(location) {
             return *occupied;
-        } else {
-            return false;
         }
+        false
     }
 
     fn update(&mut self) {
         let mut new_map = HashMap::<Coord, bool>::new();
-        for (location, occupied) in &self.seat_map {
-            // determine its status or whether it changes and add the new status to the new map
-            if *occupied {
-                // use rules for occupied seats)
+        for location in self.seat_map.keys() {
+            if let Ok(status) = self.apply_rule(location) {
+                new_map.insert(*location, status);
             }
-            else {
-                // use rule for empty setats
-            }
-            // refactor this to make it more functional
         }
         self.seat_map = new_map;
     }
 
-    fn apply_rule_for_occupied_seat_on(&self, location: &Coord) -> bool {
-        self.find_number_of_occupied_neighbors(location) < 4
-    }
-
-    fn apply_rule_for_empty_seat_on(&self, location: &Coord) -> bool {
-       self.find_number_of_occupied_neighbors(location) == 0
+    fn apply_rule(&self, location: &Coord) -> Result<bool, String> {
+        let num_neighbours = self.find_number_of_occupied_neighbors(location);
+        if let Some(status) = self.seat_map.get(location) {
+            if *status { return Ok(num_neighbours < 4); }
+            else { return Ok(num_neighbours == 0); }
+        }
+        Err("Invalid data".to_string())
     }
 
     fn find_number_of_occupied_neighbors(&self, location: &Coord) -> u8 {
         let neighbors = location.get_neighbors();
-        let mut occupied_neighbors_count: u8 = 0;
-        for neighbor in &neighbors {
-            if let Some(status) = self.seat_map.get(neighbor) {
-                if *status { occupied_neighbors_count += 1; }
-            }
-        }
-        // make this loop functional
-        occupied_neighbors_count
+        neighbors.iter().filter(|&neighbor| self.is_occupied(neighbor) ).count() as u8
+    }
+
+    // fn run_until_stabilizes(&mut self) {
+    //     let mut previous_map = &self.seat_map;
+    //     self.update();
+    //     while !previous_map.eq(&self.seat_map) {
+    //         self.update();
+    //         previous_map = &self.seat_map;
+    //         // LOOK FOR A WAY TO CHECK EQUALITY BETWEEN TWO MAPS
+    //         // IF NOT POSSIBLE, I MIGHT NEED TO LOOK FOR WAYS TO FIND OUT WHETHER A MAP HAD BEEN UPDATED
+    //     }
+    // }
+
+    fn find_occupied_seat_count(&self) -> usize {
+        self.seat_map.values().filter(|&status| *status ).count()
     }
 }
 
+// could this be simplified?
 fn create_map(filepath: &str) -> HashMap<Coord, bool> {
     let mut seats = HashMap::<Coord, bool>::new();
     if let Ok(lines) = read_lines(filepath) {
@@ -127,21 +125,21 @@ mod coord_tests {
     #[test]
     fn get_neighbors_correctly_for_middle_location() {
         let location = Coord(1, 1);
-        let mut expected_neighbors = HashSet::from([Coord(0, 0), Coord(0, 1), Coord(0, 2), Coord(1, 0), Coord(1, 2), Coord(2, 0), Coord(2, 1), Coord(2, 2)]);
+        let expected_neighbors = HashSet::from([Coord(0, 0), Coord(0, 1), Coord(0, 2), Coord(1, 0), Coord(1, 2), Coord(2, 0), Coord(2, 1), Coord(2, 2)]);
         assert_eq!(expected_neighbors, location.get_neighbors());
     }
 
     #[test]
     fn get_neighbors_correctly_for_edge_location() {
         let location = Coord(0, 1);
-        let mut expected_neighbors = HashSet::from([Coord(0, 0), Coord(0, 2), Coord(1, 0), Coord(1, 1), Coord(1, 2)]);
+        let expected_neighbors = HashSet::from([Coord(0, 0), Coord(0, 2), Coord(1, 0), Coord(1, 1), Coord(1, 2)]);
         assert_eq!(expected_neighbors, location.get_neighbors());
     }
 
     #[test]
     fn get_neighbors_correctly_for_corner_location() {
         let location = Coord(0, 0);
-        let mut expected_neighbors = HashSet::from([Coord(0, 1), Coord(1, 0), Coord(1, 1)]);
+        let expected_neighbors = HashSet::from([Coord(0, 1), Coord(1, 0), Coord(1, 1)]);
         assert_eq!(expected_neighbors, location.get_neighbors());
     }
 
@@ -175,25 +173,38 @@ mod tests {
 
     #[test]
     fn rule_for_occupied_seats_work_normally() {
-        let mut occupied_waiting_area = WaitingArea::new("test_data/all_seats_occupied.txt");
-        assert_eq!(true, occupied_waiting_area.apply_rule_for_occupied_seat_on(&Coord(0, 0)));
-        assert_eq!(false, occupied_waiting_area.apply_rule_for_occupied_seat_on(&Coord(0, 2)));
+        let occupied_waiting_area = WaitingArea::new("test_data/all_seats_occupied.txt");
+        assert_eq!(Ok(true), occupied_waiting_area.apply_rule(&Coord(0, 0)));
+        assert_eq!(Ok(false), occupied_waiting_area.apply_rule(&Coord(0, 2)));
     }
 
     #[test]
     fn rule_for_empty_seats_work_normally() {
-        let mut empty_waiting_area = WaitingArea::new("test_data/all_seats_empty.txt");
-        assert_eq!(true, empty_waiting_area.apply_rule_for_empty_seat_on(&Coord(0, 0)));
-        assert_eq!(true, empty_waiting_area.apply_rule_for_empty_seat_on(&Coord(0, 2)));
+        let empty_waiting_area = WaitingArea::new("test_data/all_seats_empty.txt");
+        assert_eq!(Ok(true), empty_waiting_area.apply_rule(&Coord(0, 0)));
+        assert_eq!(Ok(true), empty_waiting_area.apply_rule(&Coord(0, 2)));
     }
 
     #[test]
     fn can_update_entire_map() {
-        // let mut waiting_area = WaitingArea::new("test_data/all_seats_empty.txt");
-        // waiting_area.update();
-        // assert!(waiting_area.is_occupied(&Coord(0, 0)));
-        // assert!(waiting_area.is_occupied(&Coord(0, 2)));
-        // assert!(waiting_area.is_occupied(&Coord(9, 9)));
-        // assert_eq!(false, waiting_area.is_occupied(&Coord(0, 1)));
+        let mut waiting_area = WaitingArea::new("test_data/all_seats_empty.txt");
+        waiting_area.update();
+        assert!(waiting_area.is_occupied(&Coord(0, 0)));
+        assert!(waiting_area.is_occupied(&Coord(0, 2)));
+        assert!(waiting_area.is_occupied(&Coord(9, 9)));
+        assert_eq!(false, waiting_area.is_occupied(&Coord(0, 1)));
+
+        waiting_area.update();
+        assert!(waiting_area.is_occupied(&Coord(0, 0)));
+        assert_eq!(false, waiting_area.is_occupied(&Coord(0, 2)));
+        assert!(waiting_area.is_occupied(&Coord(9, 9)));
+        assert_eq!(false, waiting_area.is_occupied(&Coord(0, 1)));
     }
+
+    // #[test]
+    // fn correctly_finds_occupied_seat_count_after_map_stabalizes() {
+    //     let mut waiting_area = WaitingArea::new("test_data/all_seats_empty.txt");
+    //     waiting_area.run_until_stabilizes();
+    //     assert_eq!(37, waiting_area.find_occupied_seat_count());
+    // }
 }
