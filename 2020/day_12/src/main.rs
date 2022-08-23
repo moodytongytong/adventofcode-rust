a95f587faa43d83c89e45794d58b4a125d230437
moodytongytong <moodytongytong@users.noreply@github.com>
2022-08-23 14:19:38 +0100

    complete day 12

--- 2020/day_12/src/main.rs
+++ 2020/day_12/src/main.rs
@@ -1,7 +1,11 @@
 mod rain_risk;
 use rain_risk::Ship;
+use rain_risk::read_instructions;
 
 fn main() {
     let mut ship = Ship::new();
-    println!("The solution to part 1 is {}", ship.find_distance_from_intructions("test_data/input.txt"));
+    let instructions = read_instructions("test_data/input.txt");
+    println!("The solution to part 1 is {}", ship.find_part1_distance_from_intructions(&instructions));
+    ship.reset();
+    println!("The solution to part 2 is {}", ship.find_part2_distance_from_intructions(&instructions));
 }
--- 2020/day_12/src/rain_risk.rs
+++ 2020/day_12/src/rain_risk.rs
@@ -23,6 +23,7 @@ enum Direction {
 pub struct Ship {
     direction: Direction,
     location: Coord,
+    waypoint_location: Coord,
 }
 
 impl Ship {
@@ -30,10 +31,11 @@ impl Ship {
         Self {
             direction: Direction::E,
             location: Coord(0, 0),
+            waypoint_location: Coord(10, 1),
         }
     }
 
-    fn shift(&mut self, instruction: &str) {
+    pub fn shift(&mut self, instruction: &str) {
         let action = instruction.chars().nth(0).unwrap();
         let value = instruction[1..].parse::<usize>().unwrap();
         match action {
@@ -84,20 +86,93 @@ impl Ship {
         }
     }
 
-    pub fn find_distance_from_intructions(&mut self, instruction_filepath: &str) -> usize {
-        let mut instructions = Vec::<String>::new();
-        if let Ok(lines) = read_lines(instruction_filepath) {
-            for line in lines {
-                if let Ok(instruction) = line {
-                    instructions.push(instruction);
-                }
-            }
+    pub fn part2_shift(&mut self, instruction: &str) {
+        let action = instruction.chars().nth(0).unwrap();
+        let value = instruction[1..].parse::<usize>().unwrap();
+        match action {
+            'L' => self.turn_waypoint_left(value),
+            'R' => self.turn_waypoint_right(value),
+            'E' => self.shift_waypoint_east(value),
+            'N' => self.shift_waypoint_north(value),
+            'W' => self.shift_waypoint_west(value),
+            'S' => self.shift_waypoint_south(value),
+            _ => self.shift_relative_forward(value),
         }
+    }
+
+    fn shift_waypoint_east(&mut self, distance: usize) {
+        self.waypoint_location.0 += distance as isize;
+    }
+
+    fn shift_waypoint_north(&mut self, distance: usize) {
+        self.waypoint_location.1 += distance as isize;
+    }
+
+    fn shift_waypoint_west(&mut self, distance: usize) {
+        self.waypoint_location.0 -= distance as isize;
+    }
+
+    fn shift_waypoint_south(&mut self, distance: usize) {
+        self.waypoint_location.1 -= distance as isize;
+    }
+
+    fn shift_relative_forward(&mut self, times: usize) {
+        let delta = (self.waypoint_location.0 - self.location.0, self.waypoint_location.1 - self.location.1);
+        self.location.0 += times as isize * delta.0;
+        self.location.1 += times as isize * delta.1;
+        self.waypoint_location.0 = self.location.0 + delta.0;
+        self.waypoint_location.1 = self.location.1 + delta.1;
+    }
+
+    fn turn_waypoint_left(&mut self, value: usize) {
+        let times = value / 90;
+        for _ in 0..times {
+            let delta = (self.waypoint_location.0 - self.location.0, self.waypoint_location.1 - self.location.1);
+            self.waypoint_location.0 = self.location.0 - delta.1;
+            self.waypoint_location.1 = self.location.1 + delta.0;
+        }
+    }
+
+    fn turn_waypoint_right(&mut self, value: usize) {
+        let times = value / 90;
+        for _ in 0..times {
+            let delta = (self.waypoint_location.0 - self.location.0, self.waypoint_location.1 - self.location.1);
+            self.waypoint_location.0 = self.location.0 + delta.1;
+            self.waypoint_location.1 = self.location.1 - delta.0;
+        }
+    }
+
+    pub fn reset(&mut self) {
+        self.direction = Direction::E;
+        self.location = Coord(0, 0);
+        self.waypoint_location = Coord(10, 1);
+    }
+
+    pub fn find_part1_distance_from_intructions(&mut self, instructions: &Vec<String>) -> usize {
         for instruction in instructions {
             self.shift(&instruction[..]);
         }
         self.location.distance_from(Coord(0, 0))
     }
+
+    pub fn find_part2_distance_from_intructions(&mut self, instructions: &Vec<String>) -> usize {
+        for instruction in instructions {
+            self.part2_shift(&instruction[..]);
+        }
+        self.location.distance_from(Coord(0, 0))
+    }
+}
+
+pub fn read_instructions(filepath: &str) -> Vec<String> {
+    let mut instructions = Vec::<String>::new();
+    if let Ok(lines) = read_lines(filepath) {
+        for line in lines {
+            if let Ok(instruction) = line {
+                instructions.push(instruction);
+            }
+        }
+    }
+    instructions
 }
 
 fn convert_to_direction(direction_index: u8) -> Direction {
@@ -125,6 +200,7 @@ mod tests {
         let ship = Ship::new();
         assert_eq!(Direction::E, ship.direction);
         assert_eq!(Coord(0, 0), ship.location);
+        assert_eq!(Coord(10, 1), ship.waypoint_location);
     }
 
     #[test]
@@ -182,7 +258,61 @@ mod tests {
     #[test]
     fn find_manhattan_distance_from_start() {
         let mut ship = Ship::new();
-        let movement = ship.find_distance_from_intructions("test_data/test1.txt");
+        let instructions = read_instructions("test_data/test1.txt");
+        let movement = ship.find_part1_distance_from_intructions(&instructions);
         assert_eq!(25, movement);
+    }
+
+    #[test]
+    fn part2_turns_are_correctly_processed() {
+        let mut ship = Ship::new();
+        // left turns
+        ship.part2_shift("L90");
+        assert_eq!(Coord(-1, 10), ship.waypoint_location);
+        ship.part2_shift("L180");
+        assert_eq!(Coord(1, -10), ship.waypoint_location);
+        ship.part2_shift("L270");
+        assert_eq!(Coord(-10, -1), ship.waypoint_location);
+        ship.part2_shift("L180");
+        assert_eq!(Coord(10, 1), ship.waypoint_location);
+
+        // right turns
+        ship.part2_shift("R90");
+        assert_eq!(Coord(1, -10), ship.waypoint_location);
+        ship.part2_shift("R180");
+        assert_eq!(Coord(-1, 10), ship.waypoint_location);
+        ship.part2_shift("R270");
+        assert_eq!(Coord(-10, -1), ship.waypoint_location);
+        ship.part2_shift("R180");
+        assert_eq!(Coord(10, 1), ship.waypoint_location);
+    }
+
+    #[test]
+    fn part2_directions_correctly_processed() {
+        let mut ship = Ship::new();
+        ship.part2_shift("E1");
+        assert_eq!(Coord(11, 1), ship.waypoint_location);
+        ship.part2_shift("N2");
+        assert_eq!(Coord(11, 3), ship.waypoint_location);
+        ship.part2_shift("W3");
+        assert_eq!(Coord(8, 3), ship.waypoint_location);
+        ship.part2_shift("S4");
+        assert_eq!(Coord(8, -1), ship.waypoint_location);
+    }
+
+    #[test]
+    fn part2_forward_movement_correctly_processed() {
+        let mut ship = Ship::new();
+        ship.part2_shift("F10");
+        assert_eq!(Coord(100, 10), ship.location);
+        assert_eq!(Coord(110, 11), ship.waypoint_location);
+    }
+
+    #[test]
+    fn part2_distance_correctly_found() {
+        let mut ship = Ship::new();
+        let instructions = read_instructions("test_data/test1.txt");
+        let movement = ship.find_part2_distance_from_intructions(&instructions);
+        assert_eq!(286, movement);
     }
 }
