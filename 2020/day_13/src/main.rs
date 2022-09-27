29963b3a3086cae92199d665782c4ea4f259cf73
moodytongytong <moodytongytong@users.noreply@github.com>
2022-09-28 00:04:42 +0100

    did enough to pass the tests but need to refactor to obtain result

--- 2020/day_13/src/main.rs
+++ 2020/day_13/src/main.rs
@@ -1,8 +1,11 @@
 mod shuttle_search;
 use shuttle_search::find_wait_and_bus_id_product_from;
 use shuttle_search::create_input_holder;
+use shuttle_search::the_earliest_time;
 
 fn main() {
     let input = create_input_holder("test_data/input.txt");
-    println!("The solution to part 1 is {}", find_wait_and_bus_id_product_from(input));
+    println!("The solution to part 1 is {}", find_wait_and_bus_id_product_from(&input));
+    println!("The solution to part 2 is {}", the_earliest_time(&input[1])); 
+    // Part 2 RUNS FOR A VERY LONG TIME
 }
--- 2020/day_13/src/shuttle_search.rs
+++ 2020/day_13/src/shuttle_search.rs
@@ -6,7 +6,7 @@ use std::collections::{
     HashMap,
 };
 
-pub fn find_wait_and_bus_id_product_from(input: Vec::<String>) -> usize {
+pub fn find_wait_and_bus_id_product_from(input: &Vec::<String>) -> usize {
     let earliest_departure_time: usize = input[0].parse().unwrap();
     let mut bus_numbers = HashSet::<u16>::new();
     for word in input[1].split(",") {
@@ -37,26 +37,39 @@ pub fn create_input_holder(filepath: &str) -> Vec<String> {
     input
 }
 
-fn the_earliest_time(bus_order: &String) -> usize {
-    // constraints = [int(bus) if bus != "x" else False for bus in notes[1].split(",")]
-    // bus_times = {constraint: -i % constraint for i, constraint in enumerate(constraints) if constraint}
-    // buses = list(sorted(bus_times))
-    // slowest = buses.pop()
-    // time = bus_times[slowest]
-    // while buses:
-    //     bus = buses.pop()
-    //     while time % bus != bus_times[bus]:
-    //         time += slowest
-    //     slowest *= bus
-    // return time
+pub fn the_earliest_time(bus_order: &String) -> usize {
     let mut bus_numbers_to_order = HashMap::<u16, u8>::new();
     for (order, word) in bus_order.split(",").enumerate() {
         if let Ok(bus_number) = word.parse::<u16>() {
             bus_numbers_to_order.insert(bus_number, order as u8);
         }
     }
-    let mut ordered_buses = bus_numbers_to_order.into_keys().collect::<Vec<u16>>().sort();
-    1068781
+    let mut ordered_buses: Vec<&u16> = bus_numbers_to_order.keys().collect();
+    ordered_buses.sort();
+    ordered_buses.reverse();
+    let slowest = ordered_buses[0];
+    let slowest_order = bus_numbers_to_order[slowest];
+    ordered_buses.remove(0);
+    let mut count = 0;
+    loop {
+        count += 1;
+        let time = *slowest as usize * count - slowest_order as usize;
+        let mut bus_meeting_requirement = 0;
+        for bus in &ordered_buses {
+            if (time + bus_numbers_to_order[bus] as usize) % **bus as usize == 0 {
+                bus_meeting_requirement += 1;
+            } else {
+                break;
+            }
+        }
+        if bus_meeting_requirement == ordered_buses.len() {
+            return time;
+        }
+    }
+}
+
+pub fn the_earliest_time_clever_version(bus_order: &String) -> usize {
+    
 }
 
 
@@ -74,7 +87,7 @@ mod tests {
     #[test]
     fn correctly_find_the_product() {
         let input = create_input_holder("test_data/test1.txt");
-        let answer = find_wait_and_bus_id_product_from(input);
+        let answer = find_wait_and_bus_id_product_from(&input);
         assert_eq!(295, answer);
     }
 
@@ -83,6 +96,24 @@ mod tests {
         let input = create_input_holder("test_data/test1.txt");
         let answer = the_earliest_time(&input[1]);
         assert_eq!(1068781, answer);
+    }
+
+    #[test]
+    fn correctly_finds_part2_result_2() {
+        let answer = the_earliest_time(&"17,x,13,19".to_string());
+        assert_eq!(3417, answer);
+    }
+
+    #[test]
+    fn correctly_finds_part2_result_3() {
+        let answer = the_earliest_time(&String::from("67,7,59,61"));
+        assert_eq!(754018, answer);
+    }
+
+    #[test]
+    fn correctly_finds_part2_result_4() {
+        let answer = the_earliest_time(&String::from("1789,37,47,1889"));
+        assert_eq!(1202161486, answer);
     }
 
 }
