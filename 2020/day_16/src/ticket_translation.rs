f41444617b269a0daecdc5124c63c7a33541f9c5
moodytongytong <moodytongytong@users.noreply.github.com>
2022-12-05 20:21:06 +0000

    trying to generate a iterator from RefCell

--- 2020/day_16/src/ticket_translation.rs
+++ 2020/day_16/src/ticket_translation.rs
@@ -2,12 +2,15 @@ use std::fs::read_to_string;
 use std::collections::{
     HashSet, HashMap
 };
+use std::cell::RefCell;
 
 
 pub struct Input {
     ranges: HashMap<String, [(usize, usize); 2]>,
     my_ticket: Vec<usize>,
-    other_tickets: HashSet<Vec<usize>>,
+    other_tickets: RefCell<HashSet<Vec<usize>>>,
+    invalid_ticekts: RefCell<HashSet<Vec<usize>>>,
+    field_position: RefCell<HashMap<usize, String>>,
 }
 
 impl Input {
@@ -35,7 +38,7 @@ impl Input {
         }
     }
 
-    fn populate_other_tickets_from(&mut self, input: &str) {
+    fn populate_other_tickets_from(&self, input: &str) {
         let mut content = input.split("\n").collect::<Vec<&str>>();
         content.remove(0);
         for line in content {
@@ -45,13 +48,13 @@ impl Input {
                     ticket.push(num);
                 }
             }
-            self.other_tickets.insert(ticket);
+            self.other_tickets.borrow_mut().insert(ticket);
         }
     }
 
     pub fn find_sum_of_invalid_values(&self) -> usize {
         let mut invalid_total = 0;
-        for ticket in &self.other_tickets {
+        for ticket in self.other_tickets.borrow() {
             'number_loop: for number in ticket {
                 for (_category, range_set) in &self.ranges {
                     for range in range_set {
@@ -59,6 +62,7 @@ impl Input {
                     }
                 }
                 invalid_total += number;
+                self.invalid_ticekts.borrow_mut().insert(ticket.to_vec());
             }
         }
         invalid_total
@@ -81,13 +85,32 @@ impl Input {
         possible_categories
     }
 
-    fn find_categories_for_position(&self, position: usize) -> &str {
-        let mut result = HashSet::<&str>::from(self.ranges.keys().collect()); // NEED TO PICK UP ALL KEYS HERE
-        for ticket in &self.other_tickets {
-            let number = ticket[position];
-            result.intersection(&self.find_categories_for_number(number));
+    fn find_category_for_position(&self, position: usize) -> &str {
+        let mut result = self.ranges.keys().map(|category| category.as_str()).collect::<HashSet<_>>();
+        for known_category in self.field_position.borrow().values() {
+            result.remove(known_category as &str);
         }
-        result.iter().next().unwrap().clone()
+        for ticket in self.other_tickets.borrow() {
+            let number = ticket[position];
+            result = result.intersection(&self.find_categories_for_number(number)).copied().collect::<HashSet<_>>();
+            println!("number is {} and current result after intersection is {:?}", number, result)
+        }
+        println!("result is {:?}", result);
+        let result = result.iter().next().unwrap().clone();
+        self.field_position.borrow_mut().insert(position, result.to_string());
+        &self.field_position.borrow()[&position] as &str
+    }
+
+    fn remove_invalid_tickets(&self) {
+        for invalid_ticket in self.invalid_ticekts.borrow().iter() {
+            self.other_tickets.borrow_mut().remove(invalid_ticket as &Vec<usize>);
+        }
+    }
+
+    fn populate_field_position_map(&mut self) {
+        for position in 0..self.my_ticket.len() {
+            self.find_category_for_position(position);
+        }
     }
 }
 
@@ -95,7 +118,9 @@ pub fn create_formatted_input_from(filepath: &str) -> Input {
     let mut input = Input {
         ranges : HashMap::<String, [(usize, usize); 2]>::new(),
         my_ticket : Vec::<usize>::new(),
-        other_tickets : HashSet::<Vec<usize>>::new(),
+        other_tickets : RefCell::new(HashSet::<Vec<usize>>::new()),
+        invalid_ticekts : RefCell::new(HashSet::<Vec<usize>>::new()),
+        field_position : RefCell::new(HashMap::<usize, String>::new()),
     };
     if let Ok(raw_input) = read_to_string(filepath) {
         let three_sections: Vec<&str> = raw_input.split("\n\n").collect();
@@ -116,7 +141,7 @@ mod tests {
         let info = create_formatted_input_from("test_data/test1.txt");
         assert_eq!(3, info.my_ticket.len());
         assert_eq!(3, info.ranges.len());
-        assert_eq!(4, info.other_tickets.len());
+        assert_eq!(4, info.other_tickets.borrow().len());
 
         assert_eq!([(1, 3), (5, 7)], info.ranges["class"]);
         assert_eq!([(6, 11), (33, 44)], info.ranges["row"]);
@@ -146,6 +171,28 @@ mod tests {
     #[test]
     fn find_category_for_position_correctly() {
         let info = create_formatted_input_from("test_data/test2.txt");
-        assert_eq!("row", info.find_categories_for_position(0));
+        assert_eq!("row", info.find_category_for_position(0));
+        assert_eq!("class", info.find_category_for_position(1));
+        assert_eq!("seat", info.find_category_for_position(2));
+    }
+
+    #[test]
+    fn ensure_invalid_tickets_are_removed() {
+        let info = create_formatted_input_from("test_data/test1.txt");
+        info.find_sum_of_invalid_values();
+        info.remove_invalid_tickets();
+        assert_eq!(1, info.other_tickets.borrow().len());
+    }
+
+    #[test]
+    fn ensure_position_map_correctly_registered() {
+        let info = create_formatted_input_from("test_data/test2.txt");
+        info.populate_field_position_map();
+        let mut expected = HashMap::<usize, String>::new();
+        expected.insert(0, "row".to_string());
+        expected.insert(1, "class".to_string());
+        expected.insert(2, "seat".to_string());
+        assert_eq!(expected, info.field_position.take());
+
     }
 }
