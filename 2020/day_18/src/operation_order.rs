47ec2bd90b42637dffff025735f7614e794865d6
moodytongytong <moodytongytong@users.noreply.github.com>
2023-01-18 21:48:42 +0000

    day 17 needs work and did a tiny bit of day 18

--- 2020/day_18/src/operation_order.rs
+++ 2020/day_18/src/operation_order.rs
@@ -1,9 +1,27 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+use std::collections::LinkedList;
 
 fn evaluate(expression: &str) -> usize {
-    71
+    let mut nums: LinkedList<u8> = LinkedList::new();
+    let mut operators: LinkedList<char> = LinkedList::new();
+    for c in expression.chars() {
+        if let Some(num) = c.to_digit(10) { nums.push_back(num as u8);} 
+        else { if c != ' ' { operators.push_back(c)}; }
+    }
+    let mut current_result = nums.pop_front().unwrap() as usize;
+    for operator in operators {
+        let next_num = nums.pop_front().unwrap();
+        match operator {
+            '+' => current_result += next_num as usize,
+            '*' => current_result *= next_num as usize,
+            '-' => current_result -= next_num as usize,
+            '/' => current_result /= next_num as usize,
+            _ => println!("DO NOTHING :D"),
+        }
+    }
+    current_result
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
