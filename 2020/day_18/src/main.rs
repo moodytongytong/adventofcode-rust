542465bdcdec186ca06466fe7446643d21f5c109
moodytongytong <moodytongytong@users.noreply.github.com>
2023-02-01 22:30:43 +0000

    complete part 1

--- 2020/day_18/src/main.rs
+++ 2020/day_18/src/main.rs
@@ -1,7 +1,7 @@
 mod operation_order;
-
+use operation_order::find_sum_from_homework;
 
 fn main() {
-
+    println!("The solution to part 1 is {}", find_sum_from_homework("test_data/input.txt"));
 
 }
--- 2020/day_18/src/operation_order.rs
+++ 2020/day_18/src/operation_order.rs
@@ -4,20 +4,29 @@ use std::path::Path;
 use std::collections::LinkedList;
 
 fn evaluate(all_chars: &mut LinkedList<char>) -> usize {
-    let mut result = all_chars.pop_front().unwrap().to_digit(10).unwrap() as usize;
-    let mut first_op = ' ';
+    let mut result = if let Some(expression) = all_chars.pop_front() {
+        match expression {
+            ')' => {
+                let mut parentheses_count = 1;
+                let mut tokenized_sub_expression: LinkedList<char> = LinkedList::new();
+                let mut next_char = all_chars.pop_front().unwrap();
+                while next_char != '(' || parentheses_count != 0 {
+                    if next_char == ')' { parentheses_count += 1; }
+                    tokenized_sub_expression.push_back(next_char);
+                    next_char = all_chars.pop_front().unwrap();
+                    if next_char == '(' { parentheses_count -= 1; }
+                }
+                evaluate(&mut tokenized_sub_expression)
+            },
+            _ => expression.to_digit(10).unwrap() as usize,
+        }
+    } else {
+        0
+    };
     if let Some(op) = all_chars.pop_front() {
         match op {
             '+' => result += evaluate(all_chars) as usize,
             '*' => result *= evaluate(all_chars) as usize,
-            // ')' => {
-            //     let mut sub_expression: LinkedList<char> = LinkedList::new();
-            //     let mut next_char = all_chars.pop_front().unwrap();
-            //     while next_char != '(' {
-            //         sub_expression.push_front();
-            //         next_char = all_chars.pop_front().unwrap();
-            //     }
-            // }
             _ => println!("DO NOTHING :D"),
         }
     }
@@ -32,34 +41,17 @@ fn tokenize(expression: &str) -> LinkedList<char> {
     all_chars
 }
 
-
-
-fn evaluate_old(expression: &str) -> usize {
-    // let lh = evaluate(expression[:-2])
-    // let op = expression[-2]
-    // let rh = expression[-1]
-    // return op(lh, rh)
-    let mut nums: LinkedList<u8> = LinkedList::new();
-    let mut operators: LinkedList<char> = LinkedList::new();
-    for c in expression.chars() {
-        if let Some(num) = c.to_digit(10) { nums.push_back(num as u8);} 
-        else { if c != ' ' { operators.push_back(c)}; }
-    }
-    let mut current_result = nums.pop_front().unwrap() as usize;
-    for operator in operators {
-        let next_num = nums.pop_front().unwrap();
-        match operator {
-            '+' => current_result += next_num as usize,
-            '*' => current_result *= next_num as usize,
-            '-' => current_result -= next_num as usize,
-            '/' => current_result /= next_num as usize,
-            '(' => {
-
+pub fn find_sum_from_homework(filepath: &str) -> usize {
+    let mut results: Vec<usize> = vec![];
+    if let Ok(lines) = read_lines(filepath) {
+        for line in lines {
+            if let Ok(expression) = line {
+                let mut expression = tokenize(&expression);
+                results.push(evaluate(&mut expression));
             }
-            _ => println!("DO NOTHING :D"),
         }
     }
-    current_result
+    results.iter().sum()
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
@@ -82,7 +74,27 @@ mod tests {
 
     #[test]
     fn expression_with_parentheses_correctly_evaluated() {
-        let result = evaluate("4 * (5 + 6)");
+        let mut data = tokenize("4 * (5 + 6)");
+        let result = evaluate(&mut data);
         assert_eq!(44, result);
+    }
+
+    #[test]
+    fn expression_with_nested_parentheses_correctly_evaluated() {
+        let mut data = tokenize("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))");
+        let result = evaluate(&mut data);
+        assert_eq!(12240, result);
+    }
+
+    #[test]
+    fn very_complicated_expression_correctly_evaluated() {
+        let mut data = tokenize("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2");
+        let result = evaluate(&mut data);
+        assert_eq!(13632, result);
+    }
+
+    #[test]
+    fn find_sum_correctly() {
+        assert_eq!(26335, find_sum_from_homework("test_data/test1.txt"));
     }
 }
