d844d18250fab4d26d9525e48d28049534e57116
moodytongytong <moodytongytong@users.noreply.github.com>
2023-02-14 16:28:31 +0000

    started day 19

--- 2020/day_19/src/monster_messages.rs
+++ 2020/day_19/src/monster_messages.rs
@@ -1,6 +1,22 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+use std::collections::HashMap;
+use regex::Regex;
+
+fn digest_rules(unprocessed_rules: Vec<String>) -> HashMap<usize, String> {
+    let re = RegexSet::new(&[
+        r"\d: \"a\"",
+        r"", // piped
+        r"", // ones with only digits
+    ]).unwrap();
+
+    for rule in unprocessed_rules {
+
+    }
+}
+
+
 
 fn get_input_from(filepath: &str) -> (Vec<String>, Vec<String>) {
     let mut input: Vec<String> = vec![];
@@ -19,7 +35,6 @@ fn get_input_from(filepath: &str) -> (Vec<String>, Vec<String>) {
     (rules, messages)
 }
 
-
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path>, {
     let file = File::open(filename)?;
@@ -37,4 +52,17 @@ mod tests {
         assert_eq!(6, rules.len());
         assert_eq!(5, messages.len());
     }
+
+    #[test]
+    fn match_rules_correctly() {
+        let matcher = match("4: \"a\"");
+    }
+
+
+    // #[test]
+    // fn regex_for_quoted_rule_correctly_created() {
+    //     let (rules, messages) = get_input_from("test_data/test1.txt");
+    //     let processed_rules = digest_rules(rules);
+    //     assert_eq!("a", processed_rules[&4]);
+    // }
 }
